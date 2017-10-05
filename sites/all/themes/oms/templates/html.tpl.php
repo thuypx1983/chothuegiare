fbeef84fe7aee39dce1d4e6a1147e843547ff87a
Pham Xuan Thuy <phamxuanthuy@sutunam.com>
2017-10-05 17:33:47 +0700

    advantag

--- sites/all/themes/oms/templates/html.tpl.php
+++ sites/all/themes/oms/templates/html.tpl.php
@@ -6,7 +6,7 @@
 <meta name="MobileOptimized" content="width">
 <meta name="HandheldFriendly" content="true">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<?php print $styles; ?>
+
 <!--[if lt IE 9]><script src="<?php print base_path() . drupal_get_path('theme', 'oms') . '/js/html5.js'; ?>"></script><![endif]-->
 <?php print $scripts; ?>
 <style type="text/css">
@@ -16,7 +16,9 @@
 <body class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php print $page_top; ?>
   <?php print $page; ?>
+  <?php print $styles; ?>
   <?php print $page_bottom; ?>
+
 
   <!-- Load Facebook SDK for JavaScript -->
   <script>
